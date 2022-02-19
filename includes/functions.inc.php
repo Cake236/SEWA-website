70466206ad55902046933d6c9afe023467c6aad2
Cake <cakeandcombustiblelemons@gmail.com>
2022-02-19 09:47:23 -0600

    Add files via upload

---
+++ includes/functions.inc.php
@@ -0,0 +1,124 @@
+<?php
+
+function emptyInputSignup($phonenumber, $name, $address){
+  $result;
+
+  if(empty($phonenumber) || empty($name) ||empty($address)){
+      $result = true;
+  }
+  else {
+    $result = false;
+  }
+  return $result;
+}
+
+function phonenumberExists($conn, $phonenumber){
+  $sql = "SELECT  * FROM users WHERE usersPhonenumber = ?;";
+  $stmt = mysqli_stmt_init($conn);
+  if (!mysqli_stmt_prepare($stmt, $sql)) {
+    header("location: ../index.php?error=stmtfailed");
+    exit();
+  }
+
+  mysqli_stmt_bind_param($stmt, "s", $phonenumber);
+  mysqli_stmt_execute($stmt);
+
+  $resultData = mysqli_stmt_get_result($stmt);
+
+  if ($row = mysqli_fetch_assoc($resultData)) {
+    return $row;
+  }
+  else {
+    $result=false;
+    return $result;
+  }
+  mysqli_stmt_close($stmt);
+}
+
+function userExists($conn, $name){
+  $sql = "SELECT  * FROM users WHERE usersName = ?;";
+  $stmt = mysqli_stmt_init($conn);
+  if (!mysqli_stmt_prepare($stmt, $sql)) {
+    header("location: ../index.php?error=stmtfailed");
+    exit();
+  }
+
+  mysqli_stmt_bind_param($stmt, "s", $name);
+  mysqli_stmt_execute($stmt);
+
+  $resultData = mysqli_stmt_get_result($stmt);
+
+  if ($row = mysqli_fetch_assoc($resultData)) {
+    return $row;
+  }
+  else {
+    $result=false;
+    return $result;
+  }
+  mysqli_stmt_close($stmt);
+}
+
+function createUser($conn, $phonenumber, $name, $address){
+  $sql = "INSERT INTO users (usersPhonenumber, usersName, usersAddress) VALUES (?, ?, ?);";
+  $stmt = mysqli_stmt_init($conn);
+
+  if (!mysqli_stmt_prepare($stmt, $sql)) {
+    header("location: ../index.php?error=stmtfailed");
+    exit();
+  }
+
+
+  mysqli_stmt_bind_param($stmt, "sss", $phonenumber, $name, $address);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
+  header("location: ../index.php?error=none");
+  exit();
+}
+
+function emptyInputLogin($phonenumber){
+  $result;
+
+  if(empty($phonenumber)){
+      $result = true;
+  }
+  else {
+    $result = false;
+  }
+  return $result;
+}
+
+function loginUser($conn, $phonenumber, $connlogin, $user ){
+  $numberExists = phonenumberExists($conn, $phonenumber);
+  $number = phonenumberExists($conn, $phonenumber);
+  if ($numberExists === false) {
+    header("location: ../index.php?error=wrongLogin");
+    exit();
+  }
+  else {
+    session_start();
+    $numberExists = phonenumberExists($conn, $phonenumber);
+    $userExists = userExists($conn, $user);
+    $_SESSION["lognum"]= $phonenumber;
+    $_SESSION["logid"] = $numberExists["usersName"];
+
+    $sql = "INSERT INTO users (usersPhonenumber, usersName) VALUES (?, ?);";
+    $stmt = mysqli_stmt_init($connlogin);
+
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+      header("location: ../index.php?error=stmtfailed");
+      exit();
+    }
+
+
+    mysqli_stmt_bind_param($stmt, "ss", $_SESSION["lognum"], $_SESSION["logid"]);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+
+    session_unset();
+    session_destroy();
+    header("location: ../index.php?error=none_login");
+    exit();
+  }
+
+}
---
+++ includes/login.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+if (isset($_POST["submit1"])) {
+
+    $phonenumber = $_POST["phonenumber_login"];
+    $user = "123";
+
+
+    require_once 'dbh.inc.php';
+    require_once 'dbh1.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputLogin($phonenumber) !== false) {
+      header("location: ../index.php?error=emptyinput");
+      exit();
+    }
+
+    if(loginUser($conn, $phonenumber, $connlogin, $user) !== false){
+      header("location: ../index.php?error=none");
+      echo "You have logged in";
+      exit();
+    }
+
+}
+else if (isset($_POST["submit"])) {
+
+    $phonenumber = $_POST["phonenumber_login"];
+    $user = "123";
+
+
+    require_once 'dbh.inc.php';
+    require_once 'dbh1.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputLogin($phonenumber) !== false) {
+      header("location: ../index.php?error=emptyinput");
+      exit();
+    }
+
+    if(loginUser($conn, $phonenumber, $connlogin, $user) !== false){
+      header("location: ../index.php?error=none");
+      echo "You have logged in";
+      exit();
+    }
+
+}
+else {
+  header("location: ../index.php");
+  exit();
+}
